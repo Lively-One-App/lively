49ce958e97dda57dcfffcd99b3192f181af1a974
Artem <trekwsp@gmail.com>
2022-03-10 15:34:48 +0200

    adding support  ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
 
 import 'package:flutter/widgets.dart';
-
 import 'package:flutter/services.dart';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'src/common/bloc/app_bloc_observer.dart';
