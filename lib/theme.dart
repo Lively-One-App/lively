650b7461e3226461ad22dc001635e386fdc8d11b
Fogan1703 <blesk.bogdan@gmail.com>
2022-03-11 11:11:30 +0200

    Revert "Merge branch 'master' of https://github.com/Lively-One-App/LiveLy"
    This reverts commit 142575b999c8496add36957fbe88c3166d92b897, reversing
    changes made to 8954a761ccda4df3205cc4d58e238be1600956c2.

--- lib/theme.dart
+++ lib/theme.dart
@@ -3,22 +3,36 @@ import 'package:flutter/material.dart';
 class MyThemes {
   static ThemeData get lightTheme {
     return ThemeData(
-      primarySwatch: Colors.red,
+      primarySwatch: Colors.green,
+      splashColor: const Color(0xFF27AEA4),
+      primaryColor: const Color(0xFF27AEA4),
       appBarTheme: const AppBarTheme(
-        iconTheme: IconThemeData(color: Color(0xFF696969)),
+        titleTextStyle: TextStyle(
+            color: Color(0xFF2E2E2E),
+            fontSize: 20,
+            fontWeight: FontWeight.w400),
+        iconTheme: IconThemeData(color: Colors.black),
         elevation: 0,
-        backgroundColor: Colors.transparent,
+        backgroundColor: Color(0xFFF5F5F5),
       ),
-      fontFamily: '',
+      // fontFamily: '',
       scaffoldBackgroundColor: Colors.white,
       textTheme: const TextTheme(
-        headline1: TextStyle(
-            fontSize: 38,
-            fontWeight: FontWeight.w800,
-            color: Color(0xFFF64B4B)),
-        subtitle1: TextStyle(fontSize: 14, fontWeight: FontWeight.w800),
-      ),
-      iconTheme: const IconThemeData(color: Color(0xFFF64B4B)),
+          headline1: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+          caption: TextStyle(
+              fontSize: 14, fontWeight: FontWeight.w600, color: Colors.white),
+          bodyText1: TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+          headline2: TextStyle(
+              fontSize: 11,
+              fontWeight: FontWeight.w500,
+              color: Color(0xFF27AEA4)),
+          subtitle1: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+          subtitle2: TextStyle(fontSize: 12, fontWeight: FontWeight.w400),
+          bodyText2: TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
+          button: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: Color(0xFF828282))),
     );
   }
 }
